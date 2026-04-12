28324840d48ec4e68092b049f2915a45612fa27e
Clay <clay.degruchy+github@gmail.com>
2026-04-12 15:41:15 +0200

    init

---
+++ ModMain.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using MGSC;
+using UnityEngine;
+
+namespace QuasimorphHelloWorld
+{
+	public static class ModMain
+	{
+		private static IModContext _context;
+
+		// Items to move from ship cargo to mercenary inventory
+		private static readonly List<string> _itemIds = new List<string>
+		{
+			"water_bottle_1",
+		};
+[Hook(ModHookType.AfterBootstrap)]
+public static void OnAfterBootstrap(IModContext context)
+{
+    UnityEngine.Debug.Log("[QuickGear] Loaded, built: " + System.IO.File.GetLastWriteTime(typeof(ModMain).Assembly.Location));
+}
+
+		[Hook(ModHookType.SpaceStarted)]
+		public static void OnSpaceStarted(IModContext context)
+		{
+			_context = context;
+		}
+
+		[Hook(ModHookType.SpaceUpdateAfterGameLoop)]
+		public static void OnSpaceUpdate(IModContext context)
+		{
+			if (!Input.GetKeyDown(KeyCode.G))
+			{
+				return;
+			}
+
+			MagnumCargo cargo = context.State.Get<MagnumCargo>();
+			Mercenaries mercenaries = context.State.Get<Mercenaries>();
+
+			if (cargo == null || mercenaries == null || mercenaries.Values.Count == 0)
+			{
+				Debug.Log("[QuickGear] State not ready.");
+				return;
+			}
+
+			Mercenary merc = mercenaries.Values[0];
+			Inventory inventory = merc.CreatureData.Inventory;
+
+			foreach (string itemId in _itemIds)
+			{
+				MoveItemToInventory(cargo, inventory, itemId);
+			}
+		}
+
+		private static void MoveItemToInventory(MagnumCargo cargo, Inventory inventory, string itemId)
+		{
+			foreach (ItemStorage tab in cargo.ShipCargo)
+			{
+				for (int i = tab.Items.Count - 1; i >= 0; i--)
+				{
+					BasePickupItem item = tab.Items[i];
+					if (!item.Id.Equals(itemId))
+					{
+						continue;
+					}
+
+					tab.Remove(item);
+					if (!inventory.BackpackStore.TryPutItem(item, CellPosition.Zero))
+					{
+						// No space, put it back
+						tab.AddItemAndReshuffleOptional(item);
+						Debug.Log("[QuickGear] No space in backpack for: " + itemId);
+					}
+					else
+					{
+						Debug.Log("[QuickGear] Moved: " + itemId);
+					}
+					return;
+				}
+			}
+
+			Debug.Log("[QuickGear] Item not found in cargo: " + itemId);
+		}
+	}
+}
---
+++ note.md
@@ -0,0 +1,15 @@
+modid
+3705314255
+link
+https://steamcommunity.com/sharedfiles/filedetails/?id=3705314255
+
+create bvasics
+mod_createmanifest hello_world C:\Users\clayd\Documents\Projects\QuasimorphHelloWorld\bin\Debug\netstandard2.1
+
+create mod enytry on steam
+mod_createworkshopitem C:\Users\clayd\Documents\Projects\QuasimorphHelloWorld\bin\Debug\netstandard2.1
+
+update entry on steam
+mod_updateworkshopitem 3705314255 C:\Users\clayd\Documents\Projects\QuasimorphHelloWorld\bin\Debug\netstandard2.1 true
+
+ilspycmd -p -o "C:\Users\clayd\Documents\Projects\QuasimorphHelloWorld\Decompiled" "D:\SteamLibrary\steamapps\common\Quasimorph\Quasimorph_Data\Managed\Assembly-CSharp.dll"
